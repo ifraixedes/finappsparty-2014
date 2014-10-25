a18126fbd03cca104a1aa6e1d7aa6a4c0c7abfc0
Ivan Fraixedes Cugat <ifraixedes@melon.local>
2014-10-25 02:31:01 +0200

    Added code for voice recognition and activity to show the expense

---
+++ wear/src/main/java/financerables/myapplication/CmdWatchActivity.java
@@ -0,0 +1,65 @@
+package financerables.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CmdWatchActivity extends Activity {
+
+    private static final int SPEECH_REQUEST_CODE = 0;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onResume() {
+        processExpense("Apples 10.38");
+        super.onResume();
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+//            List<String> results = data.getStringArrayListExtra(
+//                    RecognizerIntent.EXTRA_RESULTS);
+//            processExpense(results.get(0));
+//            //
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    private void processExpense(String spokenText) {
+        Log.i("got", spokenText);
+
+        Pattern expenseInputPattern = Pattern.compile("([a-z]+) ([\\d]*(?:.[\\d]+)*)", Pattern.CASE_INSENSITIVE);
+        Matcher expenseMatcher = expenseInputPattern.matcher(spokenText);
+
+        if (expenseMatcher.matches()) {
+            String expense = expenseMatcher.group(1);
+            String amount = expenseMatcher.group(2);
+
+            Log.i("expense", expense);
+            Log.i("amount", amount);
+        }
+    }
+}
