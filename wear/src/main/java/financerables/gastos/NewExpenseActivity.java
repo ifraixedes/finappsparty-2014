2108ba6796d696e0cd71c40b12f6435463fa8916
Ivan Fraixedes <ifc@gmail.com>
2014-10-25 13:07:46 +0200

    Added everything in one activity because make sense and some ui tweaks

--- wear/src/main/java/financerables/gastos/NewExpenseActivity.java
+++ wear/src/main/java/financerables/gastos/NewExpenseActivity.java
@@ -3,13 +3,14 @@ package financerables.gastos;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.PendingResult;
@@ -23,11 +24,17 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+;
 
 
 public class NewExpenseActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener {
 
+    private static final int SPEECH_REQUEST_CODE = 0;
     public final String TAG = "NewExpenseActivity";
     public final String UID = "/expense";
     public final String DETAILS_KEY = "details";
@@ -42,10 +49,8 @@ public class NewExpenseActivity extends Activity implements ConnectionCallbacks,
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
+
         final Date date = new Date();
-        details = intent.getStringExtra(CmdWatchActivity.EXPENSE_DETAILS);
-        amount = intent.getFloatExtra(CmdWatchActivity.EXPENSE_AMOUNT, -1.0f);
         calendar = new GregorianCalendar();
         calendar.setTime(date);
 
@@ -59,12 +64,19 @@ public class NewExpenseActivity extends Activity implements ConnectionCallbacks,
             public void onLayoutInflated(WatchViewStub stub) {
                 ((TextView) stub.findViewById(R.id.date)).setText(DateFormat.getDateInstance(DateFormat.MEDIUM).format(date));
                 ((TextView) stub.findViewById(R.id.time)).setText(DateFormat.getTimeInstance(DateFormat.SHORT).format(date));
-                ((TextView) stub.findViewById(R.id.expense)).setText(details);
-                ((TextView) stub.findViewById(R.id.amount)).setText(Float.toString(amount));
             }
         });
+
+        displaySpeechRecognizer();
     }
 
+    public void showExpense(String details, String amount) {
+        TextView expenseTextView = (TextView) findViewById(R.id.expense);
+        TextView amountTextView = (TextView) findViewById(R.id.amount);
+
+        expenseTextView.setText(details);
+        amountTextView.setText(amount);
+    }
 
     public void confirmExpense(View view) {
         dataMapRequest = PutDataMapRequest.create(UID);
@@ -122,5 +134,40 @@ public class NewExpenseActivity extends Activity implements ConnectionCallbacks,
     @Override
     public void onConnectionFailed(ConnectionResult result) {
         Log.e(TAG, Integer.toString(result.getErrorCode()));
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            processExpense(results.get(0));
+            //
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    private void processExpense(String spokenText) {
+        Log.i("got", spokenText);
+
+        Pattern expenseInputPattern = Pattern.compile("([a-z]+) ([\\d]*(?:.[\\d]+)*)", Pattern.CASE_INSENSITIVE);
+        Matcher expenseMatcher = expenseInputPattern.matcher(spokenText);
+
+        if (expenseMatcher.matches()) {
+            Log.i("expense", expenseMatcher.group(1));
+            Log.i("amount", expenseMatcher.group(2));
+            showExpense(expenseMatcher.group(1), expenseMatcher.group(2));
+        }
     }
 }
